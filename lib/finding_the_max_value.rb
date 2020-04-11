c4778ed7f2990845099b65d30bbe77b066d95e85
James Jiang <monochrome473@gmail.com>
2020-04-11 21:07:41 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -6,9 +6,10 @@ def find_max_value(array)
   while array[counter] do
     if array [counter+1] > array [counter]
       max = array[counter +1]
-      binding.pry
     end
     counter += 1
   end
   return max
 end
+
+binding.pry
